903521a0aece34a612cbebf38da3c4fb81ae7475
Alice Perlov <aperlov23@gmail.com>
2020-07-03 17:46:00 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,57 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+[0,1,2], # top_row
+[3,4,5], # middle_row
+[6,7,8], # bottom_row
+[0,3,6], # left_column
+[1,4,7], # center_column
+[2,5,8], # right_column
+[0,4,8], # left_diagonal
+[6,4,2] # right_diagonal
+]
+
+ddef won?
+    WIN_COMBINATIONS.each do |combination|   #[0,1,2] which is board range (0-8)
+        if @board.cells[combination[0]] == @board.cells[combination[1]] &&
+          @board.cells[combination[1]] == @board.cells[combination[2]] &&
+          @board.taken?(combination[0]+1)
+          #Need to +1, because #taken? rspec test is working off user_input range (1-9)
+          return combination
+        end
+      end
+    return false
+  end
+
+def full?(board)
+   board.all? {|i| i == "X" || i == "O"}
+end
+
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif !won?(board) && !full?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
+
+def over?(board)
+   if draw?(board) || won?(board) || full?(board)
+     return true
+   end
+ end
+
+def over?(board)
+   if draw?(board) || won?(board) || full?(board)
+     return true
+   end
+end
+
+def winner(board)
+   if won?(board)
+      return board[won?(board)[0]]
+   end
+end
